a854c0b435d860f382a2a3148bc46d50b33dedf0
sahaj108 <kk12004@hotmail.com>
2016-03-19 09:42:30 -0400

    Create Customer.cs

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Customer
+    {
+        public int customer_id { get; set; }
+        public string customer_name { get; set; }
+
+        public double salary { get; set; }
+    }
+}
